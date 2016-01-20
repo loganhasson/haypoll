7708a422434da7e9e0d176181914bb55339b62ff
Logan Hasson <logan.hasson@gmail.com>
2016-01-19 23:35:00 -0500

    Updating graph

--- web/static/js/live_poller.js
+++ web/static/js/live_poller.js
@@ -7,6 +7,7 @@ export class LivePoller {
     let pollChannel = this._setupPollChannel()
 
     this._setupVoteButtons(pollChannel)
+    this._setupGraph()
   }
   _createSocket() {
     let socket = new Socket("/socket")
@@ -19,13 +20,12 @@ export class LivePoller {
   }
   _setupPollChannel() {
     let socket = this._createSocket()
-
     let pollId = $("#poll-id").val()
-
     let pollChannel = socket.channel("polls:" + pollId)
 
     pollChannel.on("new_vote", vote => {
       this._updateDisplay(vote.entry_id)
+      this._updateGraph()
     })
 
     pollChannel
@@ -36,15 +36,13 @@ export class LivePoller {
     return pollChannel
   }
   _updateDisplay(entryId) {
-    let self = this
-
     $.each($("li.entry"), (index, item) => {
       let li = $(item)
 
       if (entryId == li.data("entry-id")) {
         let newVotes = +(li.find(".votes").text()) + 1
 
-        self._updateEntry(li, newVotes)
+        this._updateEntry(li, newVotes)
       }
     })
   }
@@ -56,12 +54,36 @@ export class LivePoller {
       event.preventDefault()
 
       let li = $(event.currentTarget).parents("li")
-
       let entryId = li.data("entry-id")
-
       let pollId = $("#poll-id").val()
 
       pollChannel.push("new_vote", { entry_id: entryId })
     })
+  }
+  _setupGraph() {
+    google.load("visualization", "1", { packages: ["corechart"] })
+    google.setOnLoadCallback(() => {
+      this.chart = new google.visualization.PieChart(document.getElementById("my-chart"))
+      this._updateGraph()
+    })
+  }
+  _updateGraph() {
+    let data = this._getGraphData()
+    let convertedData = google.visualization.arrayToDataTable(data)
+
+    this.chart.draw(convertedData, { title: "Poll", is3D: true })
+  }
+  _getGraphData() {
+    var data = [["Choice", "Votes"]]
+
+    $.each($("li.entry"), (index, item) => {
+      let li = $(item)
+      let title = li.find(".title").text()
+      let votes = +(li.find(".votes").text())
+
+      data.push([title, votes])
+    })
+
+    return data
   }
 }
