219559c23e26cff2e4026977e51a31f5dd3de15a
Logan Hasson <logan.hasson@gmail.com>
2016-01-19 23:22:58 -0500

    Live voting

--- web/static/js/live_poller.js
+++ web/static/js/live_poller.js
@@ -5,6 +5,8 @@ export class LivePoller {
     if (!$("#poll-id").length) { return }
 
     let pollChannel = this._setupPollChannel()
+
+    this._setupVoteButtons(pollChannel)
   }
   _createSocket() {
     let socket = new Socket("/socket")
@@ -22,11 +24,44 @@ export class LivePoller {
 
     let pollChannel = socket.channel("polls:" + pollId)
 
+    pollChannel.on("new_vote", vote => {
+      this._updateDisplay(vote.entry_id)
+    })
+
     pollChannel
       .join()
       .receive("ok", resp => { console.log("Joined") })
       .receive("error", reason => { console.log("Error: ", reason) })
 
     return pollChannel
+  }
+  _updateDisplay(entryId) {
+    let self = this
+
+    $.each($("li.entry"), (index, item) => {
+      let li = $(item)
+
+      if (entryId == li.data("entry-id")) {
+        let newVotes = +(li.find(".votes").text()) + 1
+
+        self._updateEntry(li, newVotes)
+      }
+    })
+  }
+  _updateEntry(li, newVotes) {
+    li.find(".votes").text(newVotes)
+  }
+  _setupVoteButtons(pollChannel) {
+    $(".vote").on("click", event => {
+      event.preventDefault()
+
+      let li = $(event.currentTarget).parents("li")
+
+      let entryId = li.data("entry-id")
+
+      let pollId = $("#poll-id").val()
+
+      pollChannel.push("new_vote", { entry_id: entryId })
+    })
   }
 }
